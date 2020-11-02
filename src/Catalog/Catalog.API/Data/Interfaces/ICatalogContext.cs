1596eb9eb74bbd935fb25228eaf16edc48c49586
Henrique Holz de Castro <henrique.castro@limsteam.com>
2020-11-02 10:33:09 -0300

    first commit

--- src/Catalog/Catalog.API/Data/Interfaces/ICatalogContext.cs
+++ src/Catalog/Catalog.API/Data/Interfaces/ICatalogContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Catalog.API.Data.Interfaces
 {
     public interface ICatalogContext
-    {   
+    {   a
         IMongoCollection<Product> Products { get; set; }
 
     }
